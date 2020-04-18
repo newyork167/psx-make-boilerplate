5de8b026c7a94d7d8aebb124f853dfa4d917b246
Newyork167 <newyork167@gmail.com>
2020-04-18 00:39:38 -0400

    Testing CMake Custom Targets

--- remotebuild/build.bat
+++ remotebuild/build.bat
@@ -1,2 +1,1 @@
-set nc_path = D:\Development\PSX\Virtual Machine\Shared\Windows XP\\netcat-win32-1.11\netcat-1.11\nc.exe
-echo make -f Makefile | nc_path -v 192.168.0.161 4444
+echo make -f Makefile | "D:\Development\PSX\Virtual Machine\Shared\Windows XP\netcat-1.11\nc.exe" -v 192.168.0.161 4444
